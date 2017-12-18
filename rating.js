fd6742dad80afd5ac3c8d862a03dfa8c889397fa
Satyam Ghodasara <sghodas@gmail.com>
2017-12-18 12:14:20 -0500

    Updated definition of the Rating enum

--- rating.js
+++ rating.js
@@ -8,10 +8,10 @@ const Rating = {
 
 // Exports
 module.exports = Object.freeze({
-  R1: 'r1',
-  R2: 'r2',
-  R3: 'r3',
-  R4: 'r4',
-  R5: 'r5',
+  R1: Rating.R1,
+  R2: Rating.R2,
+  R3: Rating.R3,
+  R4: Rating.R4,
+  R5: Rating.R5,
   All: [Rating.R1, Rating.R2, Rating.R3, Rating.R4, Rating.R5]
 })
